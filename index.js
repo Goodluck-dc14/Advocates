afef25547d7938ebe12f24efc48f1c0b81bb4a9c
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2022-11-04 15:35:29 +0100

    added workflows

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("./utils/db");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(
